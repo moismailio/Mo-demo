518744cd3c32d3ba5220c1e0463c08b1d473c282
Mohamed-mekawy <mekawy2012@gmail.com>
2025-09-15 07:45:17 -0500

    [SCRUM-1] : dummy

--- app/src/main/java/com/demo/mo/MainActivity.kt
+++ app/src/main/java/com/demo/mo/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             MoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Hello",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
